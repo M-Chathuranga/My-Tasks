e6e776f89043df70b991a5500a9d290ba786ab13
R K M Chathuranga <matheeshachaturanga@gmail.com>
2025-02-01 11:44:41 +0530

    indexes

---
+++ picture/script.js
@@ -0,0 +1,7 @@
+let navSearch = document.getElementById("nav-search");
+let navSearchBar = document.getElementById("nav_searchbar");
+
+navSearchBar.addEventListener("click", ()=>{
+  navSearchBar.classList.toggle("active")
+})
+
